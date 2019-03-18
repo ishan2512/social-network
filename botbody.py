72131f925b9f8d8d0c1f4630d12e1e7b221cb2fb
Chirag Garg <38765776+DeVil2O@users.noreply.github.com>
2019-03-18 23:14:17 +0530

    Create botbody.py
    Frames and window using the Tkinter library.

---
+++ botbody.py
@@ -0,0 +1,18 @@
+from sys import stdin, stdout
+from tkinter import *
+import tkinter as tk
+def start(speech):
+    return True
+
+
+root=tk.Tk()
+root.geometry("1800x900+300+300")
+tk.Label(root,text="MILLION",fg="blue",font = "Verdana 72 bold").pack()
+
+root.mainloop()
+"""speech=stdin.readline()
+hello=['hello','Hello','helo','Helo','hlo','Hlo']
+if speech in hello:
+    start(speech)
+    
+"""
